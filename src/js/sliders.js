6f0ebd059deca7785b3137d566503351efc7a185
Karolina Grabowska <karolinagrabowskadev@gmail.com>
2020-05-27 15:18:08 +0200

    feature: brand slider, brand banner section, rwd

--- src/js/sliders.js
+++ src/js/sliders.js
@@ -1,3 +1,37 @@
+const brandsSlider = {
+  container: document.querySelector('.js-swiper-slider-brands'),
+  navigation: {
+    next: document.querySelector('.js-swiper-slider-brands__next'),
+    prev: document.querySelector('.js-swiper-slider-brands__prev'),
+  },
+};
+
+const brandsSliderConfig = {
+  clickableState: true,
+  slidesPerView: 2,
+  spaceBetween: 10,
+  loop: true,
+  navigation: {
+    nextEl: brandsSlider.navigation.next,
+    prevEl: brandsSlider.navigation.prev,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 6,
+    },
+  },
+};
+
+const brandsSwiper = new Swiper(brandsSlider.container, brandsSliderConfig);
+
 const benefitsSlider = {
   container: document.querySelector('.js-swiper-slider-benefits'),
   navigation: {
