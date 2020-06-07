2d429e921269e459f9873344579132a754a840f6
Karolina Grabowska <karolinagrabowskadev@gmail.com>
2020-06-07 12:29:16 +0200

    feature: mobile menu - add custom styles and scripts

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,6 +2,34 @@
   document.addEventListener(
     'DOMContentLoaded',
     function () {
+      //MOBILE MENU LIST
+      const toggleMenuMobile = {
+        emitter: document.querySelector('.js-mobile-toogler-emitter'),
+        absorber: document.querySelector('.js-mobile-toogler-adapter'),
+        overlay: document.querySelector('.js-mobile-overlay'),
+        body: document.body,
+        classToElement: 'js-navbar-scroll',
+        classToBody: 'js-overflow-hidden',
+        classVisibility: 'js-display-none',
+      };
+
+      //OPEN MOBILE MENU
+      toggleMenuMobile.emitter.addEventListener('click', (e) => {
+        toggleMenuMobile.overlay.classList.remove(toggleMenuMobile.classVisibility);
+        toggleMenuMobile.absorber.classList.remove(toggleMenuMobile.classVisibility);
+        toggleMenuMobile.absorber.classList.add(toggleMenuMobile.classToElement);
+        toggleMenuMobile.body.classList.add(toggleMenuMobile.classToBody);
+      });
+
+      //CLOSE  MOBILE MENU
+      toggleMenuMobile.overlay.addEventListener('click', (e) => {
+        toggleMenuMobile.overlay.classList.add(toggleMenuMobile.classVisibility);
+        toggleMenuMobile.absorber.classList.add(toggleMenuMobile.classVisibility);
+        toggleMenuMobile.absorber.classList.remove(toggleMenuMobile.classToElement);
+        toggleMenuMobile.body.classList.remove(toggleMenuMobile.classToBody);
+      });
+
+      //FOOTER MENU LIST
       const toggle = {
         emitterEl: document.querySelectorAll('.js-toggle-emitter'),
         emitter: 'js-toggle-emitter',
