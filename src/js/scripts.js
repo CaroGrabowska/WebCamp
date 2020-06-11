f732f48609deacf934741d2d7d5cedccebd4f5f5
Karolina Grabowska <karolinagrabowskadev@gmail.com>
2020-06-11 23:27:22 +0200

    feature: add common styles and scripts to submenus in mobile

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,20 +2,76 @@
   document.addEventListener(
     'DOMContentLoaded',
     function () {
-
+      //HIDE ELEMENT ON SCROLL
       const widget = document.querySelector('.js-widget-sale');
 
-      window.addEventListener('scroll',function () {
-         const currentScroll = window.pageYOffset;
-
-         if (currentScroll > 300) {
-           // down
-           widget.classList.add('js-display-none');
-         } else if (currentScroll < 300) {
-           // up
-            widget.classList.remove('js-display-none');
-         }
+      window.addEventListener('scroll', function () {
+        const currentScroll = window.pageYOffset;
+        if (currentScroll > 300) {
+          // scroll down
+          widget.classList.add('js-display-none');
+        } else if (currentScroll < 300) {
+          // scroll up
+          widget.classList.remove('js-display-none');
+        }
       });
+
+      //SUBMENUS TOGGLE ACTION
+
+      const menu = {
+        main: document.querySelector('.js-main-menu'),
+        parents: document.querySelectorAll('.js-menu-parent'),
+        child: '.js-menu-child',
+        mainAttr: 'data-menu-active',
+        parentAttr: 'data-parent',
+        childAttr: 'data-child',
+      };
+
+      const submenu = {
+        main: document.querySelector('.js-main-menu'),
+        parents: document.querySelectorAll('.js-submenu-parent'),
+        child: '.js-submenu-child',
+        mainAttr: 'data-submenu-active',
+        parentAttr: 'data-subparent',
+        childAttr: 'data-subchild',
+      };
+
+      menu.parents.forEach((menuitem) =>
+        menuitem.addEventListener('click', function () {
+         setMainDataState(menu, 'data-menu-active');
+         setParentDataState(this, menu, 'data-parent');
+         setClosestChildDataState(this, menu, 'data-child');
+        })
+      );
+
+      submenu.parents.forEach((submenuitem) =>
+        submenuitem.addEventListener('click', function () {
+          setMainDataState(submenu, 'data-submenu-active');
+          setParentDataState(this, submenu, 'data-subparent');
+          setClosestChildDataState(this, submenu, 'data-subchild');
+        })
+      );
+
+      function setMainDataState(menuObj,attrName) {
+        let currentMenu = menu.main;
+        let currentMenuDataAttr = currentMenu.getAttribute(attrName);
+        let dataAttrToSet = currentMenuDataAttr === 'false' ? 'true' : 'false';
+        currentMenu.setAttribute(menuObj.mainAttr, dataAttrToSet);
+      }
+
+      function setParentDataState(el, menuObj, attrName) {
+        let currentSubmenu = el;
+        let currentSubmenuDataAttr = el.getAttribute(attrName);
+        let dataAttrToSet = currentSubmenuDataAttr === 'false' ? 'true' : 'false';
+        currentSubmenu.setAttribute(menuObj.parentAttr, dataAttrToSet);
+      }
+
+      function setClosestChildDataState(parent, menuObj, attrName) {
+        let closestChild = parent.parentElement.querySelector(menuObj.child);
+        let currentChildDataAttr = closestChild.getAttribute(attrName);
+        let dataAttrToSet = currentChildDataAttr === 'false' ? 'true' : 'false';
+        closestChild.setAttribute(menuObj.childAttr, dataAttrToSet);
+      }
 
       //MOBILE MENU LIST
       const toggleMenuMobile = {
